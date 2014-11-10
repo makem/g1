f03566bd6ad7135a0ab1b97ee2143548da19ff4e
MAKEM <makem@ukr.net>
2014-11-10 18:27:51 +0200

    Test fixes

---
+++ test/oop.js
@@ -0,0 +1,12 @@
+/**
+ * Created by MAKEM on 10.11.14.
+ */
+
+var should = require ('should');
+var Histogram = require ('../histogram');
+
+describe ('Object testing', function () {
+  it('Creates a new object with closures',function(){
+
+  });
+});
